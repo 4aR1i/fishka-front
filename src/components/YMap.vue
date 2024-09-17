d573de8b99124b530f9c5a9b529d1e33e3475ab5
4aR1i <kopninn@list.ru>
2024-09-17 12:26:36 +0300

    компонент yandex map

---
+++ src/components/YMap.vue
@@ -0,0 +1,36 @@
+<template>
+  <yandex-map
+    :settings="{
+      location: {
+        center: [41.9733, 45.0428],
+        zoom: 9,
+      },
+    }"
+    height="100vh">
+    <yandex-map-default-scheme-layer :settings="{ theme: 'light' }" />
+    <yandex-map-default-features-layer />
+    <yandex-map-controls :settings="{ position: 'right' }">
+      <yandex-map-zoom-control />
+    </yandex-map-controls>
+
+    <yandex-map-marker
+      position="top-center left-center"
+      :settings="{ coordinates: [41.942982, 45.318085] }">
+      <img alt="logo" class="pin" :src="SpotIcon" />
+    </yandex-map-marker>
+  </yandex-map>
+</template>
+
+<script setup lang="ts">
+import {
+  YandexMap,
+  YandexMapControls,
+  YandexMapDefaultSchemeLayer,
+  YandexMapDefaultFeaturesLayer,
+  YandexMapZoomControl,
+  YandexMapMarker,
+} from 'vue-yandex-maps';
+import SpotIcon from '@/shared/assets/vue.svg';
+</script>
+
+<style scoped></style>
