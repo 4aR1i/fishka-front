42716bf8542a2bee7f9973e8489e1e784142bb19
4aR1i <kopninn@list.ru>
2025-03-05 23:56:43 +0300

    edit eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,6 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   {
-    files: ['**/*.vue'],
-    rules: {
-      'vue/multi-word-component-names': 'error',
-    },
-  },
-  {
     files: ['**/*.ts'],
     rules: {
       'no-console': 'warn',
